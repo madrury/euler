414ac4383a193febb6d2b4964d616abde5860ecc
madrury <matthew.drury.83@gmail.com>
2018-01-21 14:46:01 -0800

    Add data directory.

--- primes.py
+++ primes.py
@@ -15,7 +15,7 @@ def sieve(N):
 	    for j in xrange(i*i, N, i):
 	        could_be_prime[j] = False
 
-def eulerTotients(N):
+def euler_totients(N):
     '''A sieve type algorithm for calculating the Euler totients of
     the first N integers.
        
@@ -30,11 +30,11 @@ def eulerTotients(N):
     for i in xrange(2, N+1):
         # If the totient of i is 1 at this point, it is a prime.
         if totients[i] == 1:
-            # Update the totients of the multiples of i by multiplying 
+        # Update the totients of the multiples of i by multiplying 
 	    # the current value by i^(n-1)*(i - 1), where n is the 
 	    # largest power of i dividing the multiple.
 	    for j in xrange(i, N+1, i):
-		totients[j] *= (i - 1)
+		    totients[j] *= (i - 1)
 		# Guarenteed to divide out, k is an integer.
 		k = j / i
 		# Divide out the prime powers...
